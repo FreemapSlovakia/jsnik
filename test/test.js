eac679a59b4a47658b741e0498b54984e4a1154c
Martin Ždila <m.zdila@gmail.com>
2019-06-08 23:35:19 +0200

    removed sqlLayer (can be implemented as extension); added support for nested layers

--- test/test.js
+++ test/test.js
@@ -35,38 +35,39 @@ test('layer, multiple styles', (t) => {
   t.end();
 });
 
-test('sqlLayer, single style', (t) => {
-  const map = jsnik
-    .createMap({})
-    .sqlLayer('1', 'select * from table');
-  const expected =
-    '<Map>' +
-      '<Layer>' +
-        '<StyleName>1</StyleName>' +
-        '<Datasource base="db">' +
-          '<Parameter name="table">(select * from table) as foo</Parameter>' +
-        '</Datasource>' +
-      '</Layer>' +
-    '</Map>';
-  t.equal(map.stringify(), expected);
-  t.end();
-});
+// FIXME sqlLayer moved to extension (https://github.com/FreemapSlovakia/freemap-mapnik/blob/69600c5287d3b7131046c83ce7922a9e66372938/style/jsnikExtensions.js#L63)
+// test('sqlLayer, single style', (t) => {
+//   const map = jsnik
+//     .createMap({})
+//     .sqlLayer('1', 'select * from table');
+//   const expected =
+//     '<Map>' +
+//       '<Layer>' +
+//         '<StyleName>1</StyleName>' +
+//         '<Datasource base="db">' +
+//           '<Parameter name="table">(select * from table) as foo</Parameter>' +
+//         '</Datasource>' +
+//       '</Layer>' +
+//     '</Map>';
+//   t.equal(map.stringify(), expected);
+//   t.end();
+// });
 
-test('sqlLayer, multiple styles', (t) => {
-  const map = jsnik
-    .createMap({})
-    .sqlLayer(['1', '2', '3'], 'select * from table', { cacheFeatures: true });
-  const expected =
-    '<Map>' +
-      '<Layer cache-features="true">' +
-        '<StyleName>1</StyleName>' +
-        '<StyleName>2</StyleName>' +
-        '<StyleName>3</StyleName>' +
-        '<Datasource base="db">' +
-          '<Parameter name="table">(select * from table) as foo</Parameter>' +
-        '</Datasource>' +
-      '</Layer>' +
-    '</Map>';
-  t.equal(map.stringify(), expected);
-  t.end();
-});
+// test('sqlLayer, multiple styles', (t) => {
+//   const map = jsnik
+//     .createMap({})
+//     .sqlLayer(['1', '2', '3'], 'select * from table', { cacheFeatures: true });
+//   const expected =
+//     '<Map>' +
+//       '<Layer cache-features="true">' +
+//         '<StyleName>1</StyleName>' +
+//         '<StyleName>2</StyleName>' +
+//         '<StyleName>3</StyleName>' +
+//         '<Datasource base="db">' +
+//           '<Parameter name="table">(select * from table) as foo</Parameter>' +
+//         '</Datasource>' +
+//       '</Layer>' +
+//     '</Map>';
+//   t.equal(map.stringify(), expected);
+//   t.end();
+// });
